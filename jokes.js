6f4121ed9d9ed1acc7ecc5b3d6b40983fda5bc34
missagik <Analiese.Krishtalka@edu.sait.ca>
2025-03-23 18:19:00 -0600

    add html and all js components

---
+++ jokes.js
@@ -0,0 +1,62 @@
+//startup random joke
+fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    jokeText.textContent = data.joke || data.setup +"..."+ data.delivery;
+});
+
+//programming joke
+document.getElementById('programming').addEventListener("click", () => {
+    fetch("https://v2.jokeapi.dev/joke/Programming?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('jokeText').textContent = data.joke || data.setup +"..."+ data.delivery;
+    })
+    document.getElementById('titleText').textContent = "A Random Programming Joke!";
+});
+
+//spooky joke
+document.getElementById('spooky').addEventListener("click", () => {
+    fetch("https://v2.jokeapi.dev/joke/Spooky?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('jokeText').textContent = data.joke || data.setup +"..."+ data.delivery;
+    })
+    document.getElementById('titleText').textContent = "A Random Spooky Joke!";
+});
+
+//christmas joke
+document.getElementById('christmas').addEventListener("click", () => {
+    fetch("https://v2.jokeapi.dev/joke/Christmas?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('jokeText').textContent = data.joke || data.setup +"..."+ data.delivery;
+    })
+    document.getElementById('titleText').textContent = "A Random Christmas Joke!";
+});
+
+//pun
+document.getElementById('pun').addEventListener("click", () => {
+    fetch("https://v2.jokeapi.dev/joke/Pun?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('jokeText').textContent = data.joke || data.setup +"..."+ data.delivery;
+    })
+    document.getElementById('titleText').textContent = "A Random Pun!";
+});
+
+//misc
+document.getElementById('misc').addEventListener("click", () => {
+    fetch("https://v2.jokeapi.dev/joke/Miscellaneous?safe-mode")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('jokeText').textContent = data.joke || data.setup +"..."+ data.delivery;
+    })
+    document.getElementById('titleText').textContent = "A Random Miscellaneous Joke!";
+});
